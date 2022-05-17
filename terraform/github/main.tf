d308c19a5b02d6042ddc4a516ca6a461f6b6c9bb
George Dietrich <george@dietrich.app>
2022-05-17 19:41:07 -0400

    Also manage the Athena monorepo

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -98,6 +98,67 @@ resource "github_team_repository" "ci" {
   permission = "push"
 }
 
+# Athena monorepo
+
+resource "github_repository" "athena" {
+  name                   = "athena"
+  description            = "An ecosystem of reusable, independent components"
+  visibility             = "public"
+  homepage_url           = "https://athenaframework.org"
+  topics                 = ["api", "crystal", "framework"]
+  has_downloads          = false
+  has_projects           = false
+  has_wiki               = false
+  has_issues             = true
+  is_template            = false
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  allow_squash_merge     = true
+  delete_branch_on_merge = true
+  auto_init              = false
+}
+
+resource "github_branch_protection" "athena" {
+  pattern       = "master"
+  repository_id = github_repository.athena.node_id
+
+  enforce_admins          = true
+  required_linear_history = true
+
+  required_status_checks {
+    strict = true
+    contexts = [
+      "coding_standards",
+      "check_format",
+      "test (macos-latest, latest)",
+      "test (ubuntu-latest, latest)"
+    ]
+  }
+
+  required_pull_request_reviews {
+    required_approving_review_count = 0
+  }
+}
+
+resource "github_branch" "athena" {
+  branch     = "master"
+  repository = github_repository.athena.id
+}
+
+resource "github_branch_default" "athena" {
+  branch     = github_branch.athena.branch
+  repository = github_repository.athena.id
+}
+
+resource "github_team_repository" "athena" {
+  repository = github_repository.athena.id
+  team_id    = github_team.ci.id
+  permission = "triage"
+}
+
+# Components
+
 resource "github_repository" "component" {
   for_each = local.components
 
