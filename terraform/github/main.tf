10519c203e06c5b55026b3691859c67127a7241e
George Dietrich <george@dietrich.app>
2023-01-03 19:08:57 -0500

    Create skeleton repo
    Upgrade github provider

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -187,3 +187,41 @@ resource "github_team_repository" "website" {
   team_id    = github_team.ci.id
   permission = "push"
 }
+
+# App Skeleton
+
+resource "github_repository" "skeleton" {
+  name                        = "skeleton"
+  description                 = "Template repo to get up and running quickly with the Athena Framework"
+  visibility                  = "public"
+  homepage_url                = "https://athenaframework.org/"
+  topics                      = local.topics
+  has_downloads               = false
+  has_projects                = false
+  has_wiki                    = false
+  has_issues                  = true
+  is_template                 = true 
+  allow_auto_merge            = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_squash_merge          = true
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  delete_branch_on_merge      = true
+  auto_init                   = true
+  allow_update_branch         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_branch" "skeleton" {
+  branch        = "master"
+  repository    = github_repository.skeleton.id
+}
+
+resource "github_branch_default" "skeleton" {
+  branch     = github_branch.skeleton.branch
+  repository = github_repository.skeleton.id
+}
--- terraform/github/provider.tf
+++ terraform/github/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.7"
+      version = "~> 5.12"
     }
   }
 
