9692dbc5b350bb94e84c2f5e60d128a50cd4b837
George Dietrich <dev@dietrich.pub>
2025-06-24 20:34:45 -0400

    fixup! Load in additional `AWS` related resources

--- terraform/aws/variables.tf
+++
@@ -1,6 +0,0 @@
-# variable "backend_assume_role_arn" {
-#   description = "ARN of the IAM role to assume for the S3 backend"
-#   type        = string
-#   sensitive   = false
-#   nullable    = false
-# }
